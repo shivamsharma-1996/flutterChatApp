aba94363b95bd5c78a42bb49e25e51f03d9330c5
shivamsharma-1996 <shivamsharma707@ahoocom>
2019-03-24 19:00:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:simplechatapp/home_page.dart';
+
+void main() => runApp(new MyApp());
+
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    return new MaterialApp(
+      title: "Chat App",
+      home: new HomePage()
+    );
+  }
+
+
+}
